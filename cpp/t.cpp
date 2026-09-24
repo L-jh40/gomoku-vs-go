8f0a1ec783c9a85bb7d8dedb52110ff35b9e6c53
L-jh40 <1932424447@qq.com>
2026-09-25 02:08:23 +0800

    auto-sync: 2026-09-25 02:08:22

--- cpp/t.cpp
+++ cpp/t.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
-int main(){std::vector<int> v{3,1,2};std::sort(v.begin(),v.end());printf("hi %d
-",v[0]);return 0;}
+
+int main() {
+    std::vector<int> v{3, 1, 2};
+    std::sort(v.begin(), v.end());
+    printf("hi %d\n", v[0]);
+    return 0;
+}
